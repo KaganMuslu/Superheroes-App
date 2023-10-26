83596c3fc939427843449656fa3c51069e378ca6
KaganMuslu <kaanwasder@gmail.com>
2023-10-26 18:16:16 +0300

    postgresql connection

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'NO_KEY'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('URI')
     db.init_app(app)
 
     from views import views
